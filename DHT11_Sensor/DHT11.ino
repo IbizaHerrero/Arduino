c6af632949e02d9f9380e45e4197abb7f832e807
Ibiza Herrero <ibzx1992@gmail.com>
2015-09-04 23:19:51 +0200

    Create DHT11.ino
    Código para la recogida de datos del módulo para Arduino "DHT11", con las libreria "DHT.h" correspondiente para dicho módulo. A su vez, un LED se enciende al recoger una nueva lectura del sensor, como método de prueba para la verificación de la nueva lectura.

---
+++ DHT11_Sensor/DHT11.ino
@@ -0,0 +1,31 @@
+//Código de lecturas del sensor DHT11 con LED indicador para
+//interpretar el momento que se produce una lectura de datos.
+
+// Llamada a la libreria del sensor de temperatura.
+// Declaración del PIN del sensor y el tipo.
+#include "DHT.h"
+DHT dht(7, DHT11);
+
+//PIN de salida declarado para encender un LED.
+//Conexión con puerto serie e inicio del sensor.
+void setup() {
+pinMode(13, OUTPUT); 
+Serial.begin(9600);
+dht.begin();
+}
+
+void loop() {
+//Lectura de datos a las variables.
+float hum = dht.readHumidity();
+float temp = dht.readTemperature();
+
+//Se muestran los datos por el puerto serial.
+Serial.println("Humedad(%): "); Serial.println(hum);
+Serial.println("Temperatura(*c): "); Serial.println(temp);
+//Encendido de LED de lectura sucedida.
+digitalWrite(13, HIGH);
+delay(10000);
+//Apagado de LED, con delay para el comienzo de otra lectura.
+digitalWrite(13, LOW);
+delay(10000);
+}
